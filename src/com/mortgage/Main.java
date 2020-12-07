36c157cbc63095155430f47cd9afa2ed5eecd2ea
Hantonbot <75637631+Hantonbot@users.noreply.github.com>
2020-12-07 22:26:50 +0100

    Malekver

--- src/com/mortgage/Main.java
+++ src/com/mortgage/Main.java
@@ -11,53 +11,34 @@ public class Main {
         int noOfRows = input.nextInt();
 
         rhombus(noOfRows);
+        //lineOfChars(0, noOfRows, "*");
     }
 
     private static void rhombus(int noOfRows) {
         int midRow = noOfRows / 2;
 
-        int columns = 1;
+        int noOfStars = 1;
 
-        for (int i = midRow; i > 0; i--) {
-            //Printing i spaces at the beginning of each row
-            int oddMidrow = midRow + 1;
-            lineOfChars(columns, oddMidrow, " ");
-            lineOfStars(columns);
-
+        for (int i = 0; i < midRow; i++) {
+            printChars(midRow - i, ' ');
+            printChars(noOfStars, '*');
             System.out.println();
-
-            columns++;
+            noOfStars += 2;
         }
 
+        for (int i = midRow; i >= 0; i--) {
 
-        for (int i = 0; i <= columns + i; i++) {
-            //Printing i spaces at the beginning of each row
-            int oddMidrow = midRow + 1;
-            lineOfChars(columns, oddMidrow, " ");
-            lineOfStars(columns);
+            printChars(midRow - i, ' ');
+            printChars(noOfStars, '*');
             System.out.println();
-
-            columns--;
-
+            noOfStars -= 2;
         }
     }
 
-    private static void lineOfChars(int numberOfChars, int numberOfRows, String symbol) {
-
-        for (int i = 0; i < numberOfRows - numberOfChars; i++) {
-            System.out.print(symbol);
+    private static void printChars(int noOfChars, char ch) {
+        for (int i = 0; i < noOfChars; i++) {
+            System.out.print("" + ch);
         }
-
-    }
-
-    private static void lineOfStars(int row) {
-        //Return
-        int numberOfStars = row * 2 - 1;
-
-        for (int j = 0; j < numberOfStars; j++) {
-            System.out.print("*");
-        }
-
     }
 
 }
